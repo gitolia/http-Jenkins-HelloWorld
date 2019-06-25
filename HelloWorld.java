92ad89de191340763bc864d233fb41d4326b1007
gitolia <52175249+gitolia@users.noreply.github.com>
2019-06-25 19:44:23 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ class HelloWorld
 {
      public static void main(String[] args)
      {
-       System.out.println("----***** Hello World & Welcome to Jenkins Training ******-----");
+       System.out.println("----***** Tekyar und Roya ******-----");
      }
 }
 
