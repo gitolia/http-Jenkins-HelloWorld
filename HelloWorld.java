99cea6f7e29228e313ffaf4af94e656c54d87471
gitolia <52175249+gitolia@users.noreply.github.com>
2019-06-25 19:56:14 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ class HelloWorld
 {
      public static void main(String[] args)
      {
-       System.out.println("----***** Tekyar und Roya ******-----");
+       System.out.println("----***** Sasan und Zubin ******-----");
      }
 }
 
